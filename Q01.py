d8b3ca87446793986aa0f757e848db73f9d8cacf
t-shimbo <fwig1194@nifty.com>
2017-05-20 21:29:37 +0900

    add

---
+++ Q01.py
@@ -0,0 +1,32 @@
+'''
+10 進数、 2 進数、 8 進数 の いずれ で 表現 し ても 回文 数 と なる 数 の うち、 10 進数 の 10 以上 で 最小 の 値 を 求め て ください。
+
+'''
+
+def isKai(indata):
+    '''
+    kaibunn
+    :param string input:data 
+    :return: 
+    '''
+    data = indata[::-1]
+    if data == indata :
+        return True
+    return False
+
+def main(number):
+    '''
+    main
+    :param int number: 
+    :return: 
+    '''
+    d = isKai(str(number))
+    o = isKai(str(oct(number))[2::])
+    b = isKai(str(bin(number))[2::])
+    if d and o and b:
+        print(number)
+
+
+if __name__ == '__main__':
+    for n in range(11,1000,2):
+        main(n)
